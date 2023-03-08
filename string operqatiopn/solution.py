cd9a8ef6fcb7ce3c6a2511bec37ea39e2704a9cf
sahil <79046384+SahilLohan@users.noreply.github.com>
2023-03-08 22:08:56 +0530

    Update solution.py

--- string operqatiopn/solution.py
+++ string operqatiopn/solution.py
@@ -2,24 +2,29 @@
 and concatination."""
 
 #program 10.
+
 #1. String creation
 a = "Hello"
 print(a)
+
 #multiline string:-
 b="""hello,Iam Jyoti and i am a student of GJU.
 I love coding"""
 print(b)
+
 #ieration
 print(a[3])
 for i in range(6):
 print(b[i])
 print(len(a)) # string length
+
 #2. String Concatenation
 a="hello"
 b="i am jyoti"
 c=a+b
 print(c)
 print(a+" "+b)
+
 #3. deletion from string
 print(a.replace('h',''))
 print(b.replace('i',''))
